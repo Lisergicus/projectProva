74bbe18a77fd1e99ed9a85412d692ece9cb9458e
Lisergicus <pizio54@hotmail.it>
2021-10-27 12:11:47 +0200

    first commit

---
+++ main.ts
@@ -0,0 +1,13 @@
+console.log("hallo world")
+
+interface MyInterface {
+    myNumber: number;
+    myString: string;
+}
+
+let myInterface: MyInterface = {
+    myNumber: 10,
+    myString: 'Hello world',
+};
+console.log('myNumber = ', myInterface.myNumber);
+console.log('myString = ', myInterface.myString);
